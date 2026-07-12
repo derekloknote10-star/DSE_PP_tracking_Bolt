8522d368b132f0484eca6ac9104f6733d7faa0d7
derekloknote10-star <derek.lok.note10@gmail.com>
2026-07-12 11:54:34 +0800

    Updated DashboardPage.tsx

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -115,8 +115,8 @@ export default function DashboardPage() {
           loop
           muted
           playsInline
-//          className="w-20 h-20 sm:w-24 sm:h-24 object-cover rounded-xl opacity-40 shadow-sm flex-shrink-0"
-            className="h-full w-auto object-cover rounded-xl opacity-40 shadow-sm"
+          className="w-20 h-20 sm:w-24 sm:h-24 object-cover rounded-xl opacity-40 shadow-sm flex-shrink-0"
+//            className="h-full w-auto object-cover rounded-xl opacity-40 shadow-sm"
         />
       </div>
 
